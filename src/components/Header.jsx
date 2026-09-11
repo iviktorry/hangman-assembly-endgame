fe01f1ff3945b62ddff11fd67c9d1e4308ad4d39
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-11 16:14:07 +0200

    feat: add header component structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <div>
+        <p>there will be some content when game goes</p>
+      </div>
+    </div>
+  );
+}
