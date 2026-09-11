a765a23cf77014e3e4165beae91eb56c7fad062e
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-11 17:40:30 +0200

    fix: add array of object of languages, change hardcode to dynamic code

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,17 +1,17 @@
+import { languages } from "../languages";
+
 export default function Languages() {
   return (
     <section className="flex flex-wrap justify-center gap-0.5">
-      <span className="px-2 py-1 rounded-md bg-yellow-600">HTML</span>
-      <span className="px-2 py-1 rounded-md bg-yellow-600">CSS</span>
-      <span className="px-2 py-1 rounded-md bg-yellow-600">JavaScript</span>
-      <span className="px-2 py-1 rounded-md bg-yellow-600">React</span>
-      <span className="px-2 py-1 rounded-md bg-yellow-600">C++</span>
-      <span className="px-2 py-1 rounded-md bg-yellow-600">Python</span>
-      <span className="px-2 py-1 rounded-md bg-yellow-600">Ruby</span>
-      <span className="px-2 py-1 rounded-md bg-yellow-600">JavaScript</span>
-      <span className="px-2 py-1 rounded-md bg-yellow-600">C++</span>
-      <span className="px-2 py-1 rounded-md bg-yellow-600">Ruby</span>
-      <span className="px-2 py-1 rounded-md bg-yellow-600">C++</span>
+      {languages.map((item) => (
+        <span
+          key={item.name}
+          style={{ backgroundColor: item.backgroundColor, color: item.color }}
+          className="px-2 py-1 rounded-md"
+        >
+          {item.name}
+        </span>
+      ))}
     </section>
   );
 }
