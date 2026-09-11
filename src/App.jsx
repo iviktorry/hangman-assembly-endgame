73f9da9a9225c44298a3349cb7dc01fa66bf857d
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-11 16:54:32 +0200

    refactor: create Main component and move all the content there

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Header from "./components/Header";
-import Status from "./components/Status";
+import Main from "./components/Main";
 
 export default function App() {
   return (
     <div className="min-h-lvh py-8 px-4 flex flex-col items-center text-center text-white bg-neutral-800 font-custom">
-      <div className="max-w-md">
-        <Header />
-        <Status />
-      </div>
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Status from "./Status";
+
+export default function Main() {
+  return (
+    <main className="max-w-md">
+      <Header />
+      <Status />
+    </main>
+  );
+}
