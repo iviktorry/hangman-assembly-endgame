57cdc2b0ea6da7deddce7fd5f968356b0930ce95
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-12 15:49:24 +0200

    feat: create conditional render of status when the game is over

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,6 +5,7 @@ import Status from "./Status";
 import Word from "./Word";
 import NewGameButton from "./NewGameButton";
 import { useState } from "react";
+import { languages } from "../languages";
 
 export default function Main() {
   const [word, setWord] = useState("react");
@@ -13,13 +14,23 @@ export default function Main() {
   const wrongGuessCounter = clickedLetters.filter(
     (item) => !word.includes(item),
   ).length;
+  const isGameLost = languages.length - 1 <= wrongGuessCounter;
+  const isGameWon = word
+    .split("")
+    .every((item) => clickedLetters.includes(item));
+  const gameOver = isGameWon || isGameLost;
+  console.log(gameOver);
 
   return (
-    <main className="max-w-xl flex flex-col items-center gap-10 mx-auto">
-      <div className="max-w-md flex flex-col items-center gap-10">
+    <main className="max-w-xl flex flex-col items-center gap-8 mx-auto">
+      <div className="max-w-md flex flex-col items-center gap-8">
         <Header />
-        <Status />
-        <Languages />
+        <Status
+          gameOver={gameOver}
+          isGameLost={isGameLost}
+          isGameWon={isGameWon}
+        />
+        <Languages wrongGuessCounter={wrongGuessCounter} />
         <Word word={word} clickedLetters={clickedLetters} />
       </div>
       <Keyboard
--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,9 +1,22 @@
-export default function Status() {
-  const text = "test";
+export default function Status({ gameOver, isGameLost, isGameWon }) {
+  let style, title, text;
+  if (gameOver) {
+    if (isGameWon) {
+      style = "bg-green-400/50";
+      title = "You won!";
+      text = "Well done!";
+    } else if (isGameLost) {
+      style = "bg-red-400/50";
+      title = "Game over!";
+      text = "You lose! Better start learning Assembly";
+    }
+  }
+
   return (
     <section
-      className={`h-15 flex items-center justify-center w-full italic text-xl rounded-md ${text !== "" ? "bg-purple-400/70" : ""}`}
+      className={`h-17 flex flex-col items-center justify-center text-lg w-full rounded-md ${style}`}
     >
+      <p className="text-2xl font-semibold">{title}</p>
       <p>{text}</p>
     </section>
   );
