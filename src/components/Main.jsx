f099688932d0afbd484faa19af21a4eb787c220f
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-12 11:46:05 +0200

    feat: create button structure for starting new game

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,9 @@ import Header from "./Header";
 import Languages from "./Languages";
 import Status from "./Status";
 import Word from "./Word";
+import NewGameButton from "./NewGameButton";
 
 export default function Main() {
-
-    
   return (
     <main className="max-w-xl flex flex-col items-center gap-10 mx-auto">
       <div className="max-w-md flex flex-col items-center gap-10">
@@ -15,7 +14,8 @@ export default function Main() {
         <Languages />
         <Word />
       </div>
-      <Alphabet/>
+      <Alphabet />
+      <NewGameButton />
     </main>
   );
 }
