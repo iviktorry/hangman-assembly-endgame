8c2c5d65288c8acd9ad259c8e34b1736cd8fba01
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-12 14:25:09 +0200

    feat: add logic to lost languages style (less opacity)

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,15 +1,19 @@
 import { languages } from "../languages";
 
-export default function Languages() {
-  const languagesList = languages.map((item) => (
-    <span
-      key={item.name}
-      style={{ backgroundColor: item.backgroundColor, color: item.color }}
-      className="px-2 py-1 rounded-sm"
-    >
-      {item.name}
-    </span>
-  ));
+export default function Languages({ wrongGuessCounter }) {
+  const languagesList = languages.map((item, index) => {
+    return (
+      <span
+        key={item.name}
+        style={{ backgroundColor: item.backgroundColor, color: item.color }}
+        className={`px-2 py-1 rounded-sm  ${
+          index < wrongGuessCounter ? "opacity-15" : "opacity-100"
+        }`}
+      >
+        {item.name}
+      </span>
+    );
+  });
   return (
     <section className="flex flex-wrap justify-center gap-0.5">
       {languagesList}
