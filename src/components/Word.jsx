0b59568dc5648e81ec41f70ace6941124a6e25af
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-12 11:02:37 +0200

    styles: style Word component

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -2,6 +2,13 @@ export default function Word() {
   const str = "react";
   const arr = [...str];
 
-  const word = arr.map((item, index) => <span key={index}>{item}</span>);
-  return <div>{word}</div>;
+  const word = arr.map((item, index) => (
+    <span
+      key={index}
+      className="text-xl uppercase w-12 h-12 flex items-center justify-center bg-neutral-700 border-b-2"
+    >
+      {item}
+    </span>
+  ));
+  return <div className="flex gap-1">{word}</div>;
 }
