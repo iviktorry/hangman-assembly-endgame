602ed0b33f51e3ea9bcaf6fe09d8d6d99416b1ee
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-12 10:58:36 +0200

    refactor: move map code block from return

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,17 +1,18 @@
 import { languages } from "../languages";
 
 export default function Languages() {
+  const languagesList = languages.map((item) => (
+    <span
+      key={item.name}
+      style={{ backgroundColor: item.backgroundColor, color: item.color }}
+      className="px-2 py-1 rounded-md"
+    >
+      {item.name}
+    </span>
+  ));
   return (
     <section className="flex flex-wrap justify-center gap-0.5">
-      {languages.map((item) => (
-        <span
-          key={item.name}
-          style={{ backgroundColor: item.backgroundColor, color: item.color }}
-          className="px-2 py-1 rounded-md"
-        >
-          {item.name}
-        </span>
-      ))}
+      {languagesList}
     </section>
   );
 }
